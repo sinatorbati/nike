5e7b9a142183a436e902e2ee87ea3f4f0d2b6e4b
sinatorbati <sinatorbati028@gmail.com>
2025-01-12 09:16:23 +0330

    fix tiles

--- lib/components/shoe_tile.dart
+++ lib/components/shoe_tile.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:nike/models/shoe.dart';
 
 class ShoeTile extends StatelessWidget {
-  const ShoeTile({super.key});
+  final Shoe shoe;
+  const ShoeTile({super.key, required this.shoe});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(20),
-      width: 200,
-      height: 200,
-      padding: const EdgeInsets.all(20),
+      margin: const EdgeInsets.only(
+        left: 25,
+      ),
+      width: 250,
       decoration: BoxDecoration(
         color: Colors.grey[100],
         borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        children: [
+          // Image
+          Image.asset(shoe.imagePath),
+
+          //description
+          //price and name
+          //button
+        ],
       ),
     );
   }
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nike/models/shoe.dart';
 
 import '../components/shoe_tile.dart';
 
@@ -12,68 +13,82 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // سرچ بار
-        Container(
-          padding: const EdgeInsets.all(12),
-          margin: const EdgeInsets.symmetric(horizontal: 25),
-          decoration: BoxDecoration(color: Colors.grey[200]),
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'Search',
-                style: TextStyle(color: Colors.grey),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          // سرچ بار
+          Container(
+            padding: const EdgeInsets.all(12),
+            margin: const EdgeInsets.symmetric(horizontal: 25),
+            decoration: BoxDecoration(color: Colors.grey[200]),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Search',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                Icon(Icons.search, color: Colors.grey),
+              ],
+            ),
+          ),
+
+          //پیام
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25),
+            child: Text(
+              'everyone flies.. some fly longer than others',
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+          ),
+
+          //ایتم ها
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  'Hot Picks 🔥',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                ),
+                Text(
+                  'See all',
+                  style: TextStyle(color: Colors.blue),
+                )
+              ],
+            ),
+          ),
+
+          const SizedBox(
+            height: 10,
+          ),
+
+          SizedBox(
+            height: 300,
+            child: Expanded(
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: 6,
+                itemBuilder: (context, index) {
+                  //create a shoe
+                  Shoe shoe = Shoe(
+                      name: 'jordan',
+                      price: '250',
+                      imagePath: 'lib/assets/images/nike3.png',
+                      description: 'this is a shoe');
+                  return ShoeTile(
+                    shoe: shoe,
+                  );
+                },
               ),
-              Icon(Icons.search, color: Colors.grey),
-            ],
+            ),
           ),
-        ),
-
-        //پیام
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 25),
-          child: Text(
-            'everyone flies.. some fly longer than others',
-            style: TextStyle(color: Colors.grey[600]),
-          ),
-        ),
-
-        //ایتم ها
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 25),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text(
-                'Hot Picks 🔥',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-              ),
-              Text(
-                'See all',
-                style: TextStyle(color: Colors.blue),
-              )
-            ],
-          ),
-        ),
-
-        const SizedBox(
-          height: 10,
-        ),
-
-        Expanded(
-          child: ListView.builder(
-            shrinkWrap: true,
-            scrollDirection: Axis.horizontal,
-            itemCount: 5,
-            itemBuilder: (context, index) {
-              return const ShoeTile();
-            },
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
