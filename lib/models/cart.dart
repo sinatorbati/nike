b620c49a4937f6693501ca766e0ebfa223c3f1a1
sinatorbati <sinatorbati028@gmail.com>
2025-01-30 10:04:42 +0330

    add provider to .yaml

---
+++ lib/models/cart.dart
@@ -0,0 +1,50 @@
+import 'shoe.dart';
+
+class Cart {
+// list of shoes for sale
+  List<Shoe> shoeShop = [
+    Shoe(
+      name: 'Zoom FREAK',
+      price: '399',
+      description: 'The forward—thinking design of his latest signature shoe.',
+      imagePath: 'lib/assets/images/nike3.png',
+    ),
+    Shoe(
+      name: 'air max 270',
+      price: '449',
+      description:
+          'A modern take on the classic Air Max design, offering superior comfort and style.',
+      imagePath: 'lib/assets/images/nike4.png',
+    ),
+    Shoe(
+      name: 'air jordan 25',
+      price: '999',
+      description:
+          'The Air Jordan 25 is a tribute to the original Air Jordan 1.',
+      imagePath: 'lib/assets/images/nike5.png',
+    ),
+  ];
+
+// list of items in user cart
+  List<Shoe> userCart = [];
+
+// get list of shoes for sale
+  List<Shoe> getShoeList() {
+    return shoeShop;
+  }
+
+// get cart
+  List<Shoe> getUserCart() {
+    return userCart;
+  }
+
+// add items to cart
+  void addItemsToCart(Shoe shoe) {
+    userCart.add(shoe);
+  }
+
+// remove item from cart
+  void removeItemFromCart(Shoe shoe) {
+    userCart.remove(shoe);
+  }
+}
